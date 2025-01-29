6f0533a29a88b2b071f766bf5923d61aaf6dd24a
VotreNouveauNom <votre.nouvel.email@example.com>
2025-01-29 23:35:10 +0000

    requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-# Supprimer l'ancien remote
-git remote remove origin
 
-# Ajouter le nouveau remote avec votre nouveau compte
-git remote add origin git@github.com:votre-nouveau-username/projet1openclassroom.git
-
-# Vérifier la configuration
-git remote -v
